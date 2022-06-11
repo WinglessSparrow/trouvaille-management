3254e267859d6329fb423c6c4eea7fcd2d109c14
Weinzi <weinzi@pm.me>
2022-06-11 16:50:23 +0200

    scheduler

--- src/app/mitarbeiter/mitarbeiter.module.ts
+++ src/app/mitarbeiter/mitarbeiter.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
+import { SchedulerFormComponent } from './scheduler-form/scheduler-form.component';
 
 @NgModule({
   declarations: [
